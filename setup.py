c0ed330b06c1328327b63346bdbdc3c29ff3ee4b
Anshika-g-collab <anshikapunase@gmail.com>
2026-05-28 14:15:08 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup 
+
+HYPHEN_E_DOT = "-e ."
+def get_requirements(file_path:str)->list[setup]:
+    """
+    This function will return the list of requirements mentioned in the requirements.txt file
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Anshika",
+    author_email="anshikapunase@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
